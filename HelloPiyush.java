4e3f103cabc8939598e392960c125dfeb3f8399a
piyush@kotkar <abc@gmail.com>
2024-12-30 10:02:21 +0530

    Updated Code

--- HelloPiyush.java
+++ HelloPiyush.java
@@ -1,5 +1,5 @@
 public class HelloPiyush{
 public static void main(String args []){
-System.out.println("Hello,Piyush");
+System.out.println("Hello,");
 }
 }
---
+++ deploy.bat
@@ -0,0 +1,3 @@
+@echo off 
+javac HelloPiyush.java
+java HelloPiyush
